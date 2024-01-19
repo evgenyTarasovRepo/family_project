005c1e389ba55d7bc2db2a5843fde87539b8788b
Evgeny Tarasov <eatarasov@yahoo.com>
2024-01-19 17:59:34 +0300

    Child and Father entities created

---
+++ src/main/java/com/familiproject/entities/Child.java
@@ -0,0 +1,88 @@
+package com.familiproject.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "children")
+public class Child {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "father_id")
+    private Husband father;
+
+    public Child() {
+
+    }
+
+    public Child(int id, String firstName, String lastName, int age, Husband father) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.father = father;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Husband getFather() {
+        return father;
+    }
+
+    public void setFather(Husband father) {
+        this.father = father;
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/familiproject/entities/Father.java
@@ -0,0 +1,88 @@
+package com.familiproject.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "husband")
+public class Husband {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "father")
+    private List<Child> childList;
+
+    public Husband() {
+    }
+
+    public Husband(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void addChild(Child child) {
+        if (childList == null) {
+            childList = new ArrayList<>();
+        }
+        childList.add(child);
+        child.setFather(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Husband{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
